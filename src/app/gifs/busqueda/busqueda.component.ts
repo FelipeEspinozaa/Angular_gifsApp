1f43c0482fd51c9907fc4131589ed3fcec068253
FelipeEspinoza <felipe.al377@gmail.com>
2022-10-13 00:38:12 -0300

    Api Key, peticion http y muestra de gifs en pantalla

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -18,6 +18,10 @@ export class BusquedaComponent{
 
     const valor = this.txtBuscar.nativeElement.value;
 
+    if ( valor.trim().length === 0 ) {
+      return;
+    }
+
     this.gifsService.buscarGifs( valor );
 
     this.txtBuscar.nativeElement.value = "";
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private api_key: string = "td7QVoSOG8nHjJHeFxEtNcxiIRFTSIXK";
   private _historial: string[] = [];
+
+  // TODO: cambiar any por su tipo
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial]; //aca rompemos la referencia y evitamos modficar el original en caso de...
   }
 
-  buscarGifs(query: string) {
-    this._historial.unshift( query );
+
+  constructor( private http: HttpClient ) {}
+
+
+  buscarGifs(query: string = "") {
+    
+    query = query.trim().toLocaleLowerCase();
+
+    // comprobaremos si ya existe el valor en el array
+    if ( !this._historial.includes(query) ) {
+      this._historial.unshift( query );
+
+      // para mantenernos en MAX 10 elementos
+      this._historial = this._historial.splice(0, 10);
+      console.log(this._historial);
+    }
+
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=td7QVoSOG8nHjJHeFxEtNcxiIRFTSIXK&q=${query}&limit=10`)
+      .subscribe( ( resp ) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      } )
+
+
   }
   
 }
